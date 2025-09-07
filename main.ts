2ba29f20ced6639124407f18814d3281418de51a
Mises Dev <misesdev@onionmail.org>
2025-09-07 05:07:41 -0300

    configured docker compose

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
-
-// import cron from "node-cron";
+import cron from "node-cron";
 import runIndexer from ".";
 
-// // Executa todo dia às 00:00
-// cron.schedule("0 0 * * *", async () => {
-//     console.log("Executando indexador às 00:00...");
-//     await runIndexer()
-// });
+// Executa todo dia às 00:00
+cron.schedule("0 0 * * *", async () => {
+    console.log("Executando indexador às 00:00...");
+    await runIndexer()
+});
 
-runIndexer()
